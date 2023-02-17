da17e47625ab9e00644271ed5db0912e4b5dcbbf
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-16 19:21:19 -0800

    different size of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of an int: %1d byte(s)\n", Sizeof(int));
-	printf("Size of a long int: %1d byte(s)\n", Sizeof(long int));
-	printf("Size of a float: %1d byte(s)\n", Sizeof(float));
-	printf("Size of a double:  %1d byte(s)\n", Sizeof(double));
-	printf("Size of a char: %1d byte(s)\n", Sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of a float: %1d byte(s)\n", sizeof(float));
+	printf("Size of a long long int:  %1d byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
 	return (0);
 }
 
