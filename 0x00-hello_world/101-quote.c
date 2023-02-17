aeacb865d92cb77fd43d89644590154a5ed7d6fa
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-16 19:42:39 -0800

    compilinf file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, printing standard errot
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
