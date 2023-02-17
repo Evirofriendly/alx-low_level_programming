0c820edc7e47a4da908b02c77559b18a5af9451c
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-17 13:21:00 -0800

    printing nunber

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry program point printing numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0 ; y < 10 ; y++)
+	{
+		printf("%d", y);
+		printf("\n");
+	}
+	return (0);
+}
