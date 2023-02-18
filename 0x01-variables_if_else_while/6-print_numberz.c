bcd9cbe728080f60608f601b8cc6141a668b424c
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-17 19:26:21 -0800

    #

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0 ; y < 10 ; y++)
+		putchar((x % 10 + '0'));
+	putchar('\n');
+	return (0);
+}
