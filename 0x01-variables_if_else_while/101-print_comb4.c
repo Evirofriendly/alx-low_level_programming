f64a132fdee8e4daee9d281871a8f92d93d8091f
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-19 07:40:48 -0800

    printing three digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 
 /**
  * main - Entry program point
@@ -15,27 +14,27 @@ int main(void)
 
 	/*loop the first digi */
 
-	for (x = 0 ; x <= 8 ; x++)
+	for (x = 0 ; x <= 7 ; x++)
 	{
 		/* loop the second digit */
-		for (y = x + 1 ; y <= 9 ; y++)
+		for (y = x + 1 ; y <= 8 ; y++)
 		{
 			/* loop the third digit */
 
-			for (z = y + 1 ; z <= 10 ; z++)
+			for (z = y + 1 ; z <= 9 ; z++)
 			{
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
 
 				/* continue adding comma, and space */
-				if (x == 8 && y == 9 && z == 10)
+				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
