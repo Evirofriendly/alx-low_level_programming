2c22b2922bda5a9af25e46a93fbcb87676a38c28
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-19 06:49:45 -0800

    printing two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,3 +8,30 @@
  */
 
 int main(void)
+{
+	/* loop through a two digit mumbers*/
+
+	int x, y;
+
+	/* loop throgh the first digit */
+
+	for (x = 0 ; x <= 8 ; x++)
+	{
+		/* loop throuh the second digit */
+
+		for (y = x + 1 ; y <= 9 ; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y & 10) + '0');
+
+			/* continue adding comma and space */
+
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
