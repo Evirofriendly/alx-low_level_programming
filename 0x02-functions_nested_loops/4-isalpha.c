3587563bee04f8f57d161116187c7067a0a6dd95
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-21 13:50:30 -0800

    #

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - printing character in upercase
+ * @c: check for character
+ *
+ * Return: 1 if is lower or upper case otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
