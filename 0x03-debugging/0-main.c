6c1646b5b6655ba640d520e038cf6d88f83d9041
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-22 07:48:23 -0800

    #

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints positive or negative integer
+ *
+ * Return: Always 0 (successs)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,6 @@
+#debugging project
+- The following project files are for learning how to debug a code
+
+- main.h: contain project prototype function of the project
+
+- 
