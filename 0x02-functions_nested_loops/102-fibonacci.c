5f3edc12973f25c877d7245ff3a998ad9031d0aa
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-21 18:22:57 -0800

    #

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point first 50 number
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int m;
+	unsigned long f1 = 0, f2 = 1, fm2;
+
+	for (m = 0; m < 50; m++)
+	{
+		fm2 = f1 + f2;
+		printf("%lu", fm2);
+
+		f1 = f2;
+		f2 = fm2;
+
+		if (m == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
