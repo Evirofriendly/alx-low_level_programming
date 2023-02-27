5f90f1c23590e8f608580c2f451b093d7bcb8301
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-27 11:27:36 -0800

    swap two integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integer
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change; /*used to change the storage empty space */
+
+	change = *a;
+
+	a* = b*;
+
+	b* = change;
+}
