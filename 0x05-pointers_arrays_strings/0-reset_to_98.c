f52131a7fe2e9f9fe43125d735d9d94e998ed7a6
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-27 11:16:14 -0800

    pointer array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,9 @@
 /**
  * reset_to_98 - updating int to parameter and pointing to an address.
  * @n: integer to point
- *
- * Return: 0 if successefull
  */
 
 void reset_to_98(int *n)
 {
-	int *n = 98;
+	*n = 98;
 }
