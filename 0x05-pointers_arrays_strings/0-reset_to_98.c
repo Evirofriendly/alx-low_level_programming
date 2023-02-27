6f1b41fe8870080631ae8028a8b083431e43e670
Evirofriendly <kunlexeddyone@gmail.com>
2023-02-27 09:32:20 -0800

    pointer and array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updating int to parameter and pointing to an address.
+ * @*n: integer to point
+ *
+ * Return: 0 if successefull
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n;
+}
