f2386733070d78d9c4dc429c84b0b162af576f1f
Evirofriendly <kunlexeddyone@gmail.com>
2023-03-02 05:23:56 -0800

    concantenate a string function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenate a two string
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j = strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i + j] = src[i];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
