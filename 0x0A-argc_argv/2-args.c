fda0dfa9eacb2b91072f639c3dfc158af01e4836
Evirofriendly <kunlexeddyone@gmail.com>
2023-03-10 09:51:14 -0800

    task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
- * @argc: Argument count/number.
+ * main - Prints all arguments
+ * @argc: Argument count/number of argument inputed
  * @argv: Argument vector , array of pointers to the arguments.
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
+ *
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
+
 {
-	int i, n1, n2;
+	int i = 0;
 
-	if (argc != 3)
+	for (i = 0 ; i < argc ; i++)
+
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	i = n1 * n2;
-
-	printf("%d\n", i);
-
 	return (0);
 }
