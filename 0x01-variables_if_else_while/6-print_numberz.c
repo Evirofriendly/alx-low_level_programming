e93950da793db86843fa62588230f68899cd911e
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-06 16:35:51 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet with character
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int f;
+
+	for (f = 0; f < 10; f++)
+	{
+		putchar('0' + f);
+	}
+	putchar('\n');
+	return (0);
+}
