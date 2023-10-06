5ffc9934d5717234e8b2463ae14f4e62f74768cc
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-06 15:49:44 -0700

    alphabet with exceptional)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet
+ *
+ * Return: Always 0 if Success
+ */
+
+int main(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++)
+	{
+		if (e != 'q' && e != 'e')
+		{
+			putchar(e);
+		}
+	}
+	putchar('\n');
+	return (0);
+		
+}
