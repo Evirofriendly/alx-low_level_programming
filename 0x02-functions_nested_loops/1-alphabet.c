69e6d1994208f277550a4c26802368c0ddb36b09
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-10 07:22:55 -0700

    printing alhabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all the alphabet in lower case
+ *
+ * Return: NULL
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
