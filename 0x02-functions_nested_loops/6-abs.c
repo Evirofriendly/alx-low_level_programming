5fa2cc77b5b50c5399370c410af489197fe53367
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-10 12:02:26 -0700

    absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - printing absolute value of interger
+ *
+ * Return: integer
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1);
+	}
+}
