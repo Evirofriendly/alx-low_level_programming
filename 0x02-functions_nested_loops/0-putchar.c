2e9573476023585a24583edf9a450a332a315084
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-10 07:02:02 -0700

    prototype

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar('t');
+	putchar('c');
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
+	return (0);
+}
