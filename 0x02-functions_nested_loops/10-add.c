3d3908e6c55293f587864001803899d7ef5f7e34
Evirofriendly <kunlexeddyone@gmail.com>
2023-10-11 07:01:56 -0700

    addition of integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - printing addition of two integer
+ * @i: first integer to add
+ * @j: second integer to add
+ *
+ * Return: addition of integer
+ */
+
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+
+	return (result);
+}
